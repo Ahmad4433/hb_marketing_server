cceb0333186c64903c8cd978f58803918ae4cbc7
ahmad4433 <aliraza126005@gmail.com>
2024-08-21 01:31:04 +0500

    gdfgdf

--- routes/getNum.js
+++ routes/getNum.js
@@ -8,22 +8,33 @@ const getNumber = async (req, res, next) => {
 
 
     try {
+        let newArray = [];
         let count;
         const page = await Page.find().countDocuments();
+
         if (page === 0) {
             count = 1
         } else {
             count = page
         }
         const limit = 50;
-        const list = await Numbers.find().skip((count -1)  *limit).limit(limit)
+        const list2 = await Numbers.find()
+        list2.map((item) => {
+            return item.list.map((item2) => {
+                return newArray.push(item2)
+            })
+        }).flat()
+
+
+        const formatedPagination = newArray.slice((count - 1) * limit, count * limit)
+     
 
         const newPage = new Page({
             page: 1
         })
 
         await newPage.save()
-        res.status(200).json({ message: 'success', status: true, list })
+        res.status(200).json({ message: 'success', status: true, list: formatedPagination })
     } catch (error) {
         next(error)
     }
@@ -34,3 +45,7 @@ const getNumber = async (req, res, next) => {
 }
 
 module.exports = getNumber
+
+
+
+
